56834f438beeedccb1eb5a4f2d95ad7b11d2afac
spidergwin <gwinoffiicial1@gmail.com>
2026-02-10 22:20:30 +0100

    start creating layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -27,15 +27,29 @@ function RouteComponent() {
   })
 
   return (
-    <>
-      <div className="bg-blue-400 w-full"></div>
-      <div>
+    <div className="flex gap-4 min-h-screen md:flex-col">
+      <div className="bg-blue-400 w-full h-full rounded-md">
+        <div className="flex md:flex-col">
+          {
+            formData.map(({ id, stepLabel, label }) => (
+              <div key={id} className="flex gap-2 items-center justify-center">
+                <div className={`border-2 rounded-full border-white flex place-content-center ${id === formState.id ? "bg-blue-400" : "bg-transparent"} `}>{id}</div>
+                <div className="max-md:hidden">
+                  <p>{stepLabel}</p>
+                  <p>{label.toUpperCase()}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+      <div className="flex-3">
         <h1>{formState.name}</h1>
         <p>{formState.description}</p>
         <form>
           <formState.component form={form} />
         </form>
       </div>
-    </>
+    </div >
   )
 }
